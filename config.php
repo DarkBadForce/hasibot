d3cb0710c9fba8275eb42315d5c4a361accb95af
MateLing <627957108@qq.com>
2016-09-29 00:07:27 +0800

    Update and rename config.default.php to config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 // 把自己的配置放到config.php吼不吼啊？
 $config = array(
-	"key" => "API key放在这里你们资瓷不资瓷啊？",
-	"admin" => array( "管理猿的ID放在这里吼不吼啊？" ),
+	"key" => "260817",
+	"admin" => array( "zehengling" ),
 	"savefile" => __DIR__ . "/life.json"
 );
 
